b01b6f902da35231e9d0980303453b97d0583b7c
user <w2016@e-mirim.hs.kr>
2021-07-21 09:36:49 +0900

    commit 1

--- app/src/main/java/kr/hs/emirimw2016/project10_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirimw2016/project10_3/MainActivity.java
@@ -3,6 +3,8 @@ package kr.hs.emirimw2016.project10_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btncall = findViewById(R.id.btn_call);
+        Button btnfinish = findViewById(R.id.btn_finish);
+
+        btncall.setOnClickListener(btnListener);
+        btnfinish.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case  R.id.btn_call:
+                    break;
+                case R.id.btn_finish:
+                    break;
+            }
+        }
+    };
 }
